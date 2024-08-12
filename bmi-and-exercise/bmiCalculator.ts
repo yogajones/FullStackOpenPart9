fa64b0492f09ee46ac05c34b75bbbb5c3809b7c0
yogajones <95644662+yogajones@users.noreply.github.com>
2024-08-12 13:09:55 +0300

    Ex 9.1

---
+++ bmi-and-exercise/bmiCalculator.ts
@@ -0,0 +1,23 @@
+const calculateBmi = (height: number, weight: number): string => {
+  const bmi: number = (weight / (height * height)) * 10000;
+
+  if (bmi < 18.5) {
+    return "Underweight";
+  } else if (bmi >= 18.5 && bmi < 24.9) {
+    return "Normal range";
+  } else if (bmi >= 25 && bmi < 29.9) {
+    return "Overweight";
+  } else {
+    return "Obese";
+  }
+};
+
+try {
+  console.log(calculateBmi(180, 74));
+} catch (error: unknown) {
+  let errorMessage = "Something went wrong: ";
+  if (error instanceof Error) {
+    errorMessage += error.message;
+  }
+  console.log(errorMessage);
+}
