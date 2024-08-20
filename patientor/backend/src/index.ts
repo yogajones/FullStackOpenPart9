6842a2f63c72dc3574d10c04b123c2fee662584e
yogajones <95644662+yogajones@users.noreply.github.com>
2024-08-20 12:39:43 +0300

    Ex 9.11

--- patientor/backend/src/index.ts
+++ patientor/backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import diagnosisRouter from "./routes/diagnoses";
+import patientRouter from "./routes/patients";
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.get("/api/ping", (_req, res) => {
 });
 
 app.use("/api/diagnoses", diagnosisRouter);
+app.use("/api/patients", patientRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
---
+++ patientor/backend/src/services/patientService.ts
@@ -0,0 +1,28 @@
+import patientData from "../../data/patients";
+import { Patient, NonSensitivePatient } from "../types";
+
+const patients: Patient[] = patientData;
+
+const getPatients = (): Patient[] => {
+  return patients;
+};
+
+const getNonSensitivePatients = (): NonSensitivePatient[] => {
+  return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation,
+  }));
+};
+
+const addPatient = () => {
+  return null;
+};
+
+export default {
+  getPatients,
+  getNonSensitivePatients,
+  addPatient,
+};
