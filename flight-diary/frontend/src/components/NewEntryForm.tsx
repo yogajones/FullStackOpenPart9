575f9fd28e3344c4bc33235c652c0016cad2ef40
yogajones <95644662+yogajones@users.noreply.github.com>
2024-08-25 12:51:03 +0300

    Ex 9.18

--- flight-diary/frontend/src/components/NewEntryForm.tsx
+++ flight-diary/frontend/src/components/NewEntryForm.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState } from "react";
 import { createEntry } from "../services/diaryService";
 import { Visibility, Weather } from "../types";
@@ -7,24 +8,35 @@ export const NewEntryForm = () => {
   const [weather, setWeather] = useState("");
   const [visibility, setVisibility] = useState("");
   const [comment, setComment] = useState("");
+  const [error, setError] = useState<string | null>(null);
 
   const handleNewEntry = async (event: React.SyntheticEvent) => {
     event.preventDefault();
-    await createEntry({
-      date: date,
-      weather: weather as Weather,
-      visibility: visibility as Visibility,
-      comment: comment,
-    });
-    setDate("");
-    setWeather("");
-    setVisibility("");
-    setComment("");
+    try {
+      await createEntry({
+        date: date,
+        weather: weather as Weather,
+        visibility: visibility as Visibility,
+        comment: comment,
+      });
+      setDate("");
+      setWeather("");
+      setVisibility("");
+      setComment("");
+      setError(null);
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response) {
+        setError(error.response.data);
+      } else {
+        setError("An unexpected error occurred.");
+      }
+    }
   };
 
   return (
     <div>
       <h2>Add new entry</h2>
+      {error && <p style={{ color: "red" }}>{error}</p>}{" "}
       <form onSubmit={handleNewEntry}>
         {" "}
         date:{" "}
